45f97360bd45f64a0e0c7cc181b90f2603ab118c
1198159 <jeffastedman@gmail.com>
2020-07-23 16:23:53 -0700

    first commit 2

--- RobotLibrary/src/main/java/com/technototes/robotlibrary/RobotLibrary.java
+++ RobotLibrary/src/main/java/com/technototes/robotlibrary/RobotLibrary.java
@@ -1,9 +1,10 @@
 package com.technototes.robotlibrary;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class RobotLibrary extends LinearOpMode {
-
+    public static DcMotor test;
     @Override
     public void runOpMode() throws InterruptedException {
 
